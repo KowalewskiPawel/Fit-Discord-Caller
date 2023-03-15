6bdacb066ba8d0905c7baddeb320a10288c4ef27
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-03-15 23:55:55 +0100

    add google client

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import dotenv from 'dotenv';
+import { google } from "googleapis";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3008;
+
+app.get('/api/getURL', (req, res) => {
+    const oauthClient = new google.auth.OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URI
+    )
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
